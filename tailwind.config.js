15b05c74520661fc2e92f6b667789e0b58b3d2e7
mehdi660 <mehdi.belgoumrii@gmail.com>
2024-07-23 14:37:07 +0200

    :lipstick: working on style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Montserrat",
+          "Roboto",
+          "Open Sans",
+          "Lato",
+          "Poppins",
+          "sans-serif",
+        ],
+      },
+    },
+    colors: {
+      "semi-light-gray": "rgba(255, 255, 255, 0.5)", // Couleur semi-claire
+    },
   },
   plugins: [],
 };

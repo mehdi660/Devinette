5010ddb9c6ed21ed9eac37962b5da13af6a39d6d
Mehdi <mehdi.belgoumrii@gmail.com>
2024-07-29 17:17:44 +0000

    :lipstick: changing colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Question from "./components/Question";
 function App() {
   return (
     <div>
-      <h1 className=" font-semibold text-4xl p-10">Quizz Game !</h1>
+      <h1 className=" font-semibold text-4xl p-10 ">Quizz Game !</h1>
       <Question />
     </div>
   );
